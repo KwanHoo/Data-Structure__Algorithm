edb02c2a7f9a6aa29282862fd758bff2cba7c4eb
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2022-04-06 17:05:41 +0900

    BFS _ 너비 우선 탐색

---
+++ Algorithm/DFS,BFS/bfs.py
@@ -0,0 +1,42 @@
+## BFS(Breadth-First Search)
+## 너비 우선 탐색: 가까운 노드부터 탐색, 큐 이용
+## 구현 방법 : 큐 자료구조 이용 (deque)
+from collections import deque
+
+##* BFS 메서드 정의
+def bfs(graph, start, visited):
+    ##* 큐 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    ##* 현재 노드를 방문 처리
+    visited[start] = True
+    ##* 큐가 빌 떄까지 반복
+    while queue:
+        ##* 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        ##* 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽임
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현 (2D 리스트)
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 각노드가 방문된 정보를 리스트 자료형으로 표현 (1D 리스트)
+visited = [False] * 9
+print(visited)
+# 정의된 DFS 함수 호출로
+bfs(graph, 1, visited)
+print(visited)
