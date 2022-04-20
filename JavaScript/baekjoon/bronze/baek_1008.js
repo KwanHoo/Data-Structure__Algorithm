b397cdbda85a50b87fd4aa95c6a7a74083f9a544
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2022-04-20 23:26:56 +0900

    백준 1008번 _ A/B
    - 입출력

---
+++ JavaScript/baekjoon/bronze/baek_1008.js
@@ -0,0 +1,12 @@
+// 백준 1008번
+// A/B
+// 입출력
+
+//* JS에서 콘솔을 통해 값을 입력 받기 위해서는 readlin 모듈 이용가능
+//! 하지만 readline 모듈 보다는 fs 모듈 사용할것!
+
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a / b);
