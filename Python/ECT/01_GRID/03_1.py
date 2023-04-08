8b94867e7c7e2f5f00c54b77c723b74839d6966a
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-04-08 17:41:55 +0900

    그리디_숫자카드게임
    Greedy 문제
    3. 숫자카드게임

--- Python/ECT/01_GRID/03_1.py
+++ Python/ECT/01_GRID/03_1.py
@@ -1,18 +1,19 @@
 # 숫자 카드 게임
 # 하
 # 그리드
-# min() 함수를 이용한 답안
+## 2중 반복문 구조를 이용하는 답안
 
-# N, M을 공백으로 구분하여 입력받기
 N, M = map(int, input().split())
 
 result = 0
-# 한줄 씩 입력 받아 확인
+# 한 줄 씩 입력받아 확인
 for i in range(N):
     data = list(map(int, input().split()))
     # 현재 줄에서 '가장 작은 수' 찾기
-    min_value = min(data)
-    # '가장 작은 수' 들 중에서 가장 큰수 찾기
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
+    # '가장 작은 수'들 중에서 가장 큰 수 찾기
     result = max(result, min_value)
 
 print(result) # 최종 답안 출력
