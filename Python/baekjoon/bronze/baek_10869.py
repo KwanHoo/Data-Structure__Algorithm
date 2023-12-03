58bb1f058462b46d5785dfc216f4478f63c81417
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-12-03 14:42:58 +0900

    백준_10869 사칙연산

---
+++ Python/baekjoon/bronze/baek_10869.py
@@ -0,0 +1,17 @@
+# 문제집) Python 배우기 (1~50)
+## 10869_사칙연산
+# 브론즈 5
+
+A, B = map(int, input().split())
+
+plusV = A + B
+minV = A - B
+mulV = A * B
+mokV = A // B
+namV = A % B
+
+print(plusV)
+print(minV)
+print(mulV)
+print(mokV)
+print(namV)
