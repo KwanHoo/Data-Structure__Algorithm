22fe57513f24e8fac66f59ab18c4bca382898b41
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-12-14 22:09:44 +0900

    백준_2935 소음

---
+++ Python/baekjoon/bronze/baek_2935.py
@@ -0,0 +1,18 @@
+# 문제집) Python 배우기 (1~50)
+## 2935_소음
+# 브론즈 3
+
+A = int(input())
+
+yun = str(input())
+
+B = int(input())
+
+if yun == '+':
+    result = A + B
+elif yun == '*':
+    result = A * B
+else:
+    result = 'not'
+
+print(result)
