4bab5e824507668fb24844bfd610d99a5da559a0
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2024-01-02 23:21:17 +0900

    PGM_SqlKIT_SELECT_조건에 맞는 리스트 출력하기

--- SQL/Programmers/Kit/SELECT/01.sql
+++ SQL/Programmers/Kit/SELECT/01.sql
@@ -2,6 +2,7 @@
 -- SQL 고득점 Kit : SELECT
 -- 조건에 맞는 도서 리스트 출력하기
 
+-- ORACLE
 select
     book_id,
     to_char(published_date, 'yyyy-mm-dd') as published_date
@@ -11,3 +12,11 @@ where
     and category ='인문'
 order by published_date asc
 ;
+
+-- MySQL
+
+SELECT BOOK_ID,
+DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
+FROM BOOK
+WHERE CATEGORY = '인문' AND YEAR(PUBLISHED_DATE) = 2021
+ORDER BY PUBLISHED_DATE;
