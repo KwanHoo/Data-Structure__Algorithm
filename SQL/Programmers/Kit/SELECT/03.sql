4c0b88e9a6eaaad20a0b8379d3494c6c7a05e870
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2024-01-02 23:34:11 +0900

    PGM_SqlKIT_SELECT_흉부외과 또는 일반외과 의사 목록 출력하기

---
+++ SQL/Programmers/Kit/SELECT/03.sql
@@ -0,0 +1,18 @@
+-- 프로그래머스
+-- SQL 고득점 Kit : SELECT
+-- 흉부외과 또는 일반외과 의사 목록 출력하기
+
+-- 의사이름, 의사IOD, 면허번호, 고용일자, 진료과코드. 전화번호
+-- SELECT * FROM DOCTOR;
+-- 흉부 CS, 일반, GS인
+
+-- Oracle
+select
+dr_name as DR_NAME,
+dr_id,
+mcdp_cd,
+to_char(hire_ymd ,'yyyy-mm-dd')
+from DOCTOR
+where mcdp_cd in('CS','GS')
+order by
+    hire_ymd desc, dr_name asc
