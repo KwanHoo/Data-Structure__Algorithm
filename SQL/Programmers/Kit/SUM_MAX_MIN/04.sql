474abdb6858ce946ba46533a09fc8850ac1a08bb
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2024-01-04 21:53:09 +0900

    PGM_SqlKIT_SMM_최솟값 구하기

---
+++ SQL/Programmers/Kit/SUM_MAX_MIN/04.sql
@@ -0,0 +1,13 @@
+-- 프로그래머스
+-- SQL 고득점 Kit : SUM, MAX, MIN
+-- 최솟값 구하기
+
+-- ORACLE
+SELECT
+    MIN(DATETIME) AS "시간"
+FROM ANIMAL_INS
+
+-- MySQL
+SELECT
+    MIN(DATETIME) AS "시간"
+FROM ANIMAL_INS
