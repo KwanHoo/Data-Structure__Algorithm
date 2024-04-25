eb406fb3a06d40f34f525d0e09032cdc6dd7278e
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2024-04-26 00:00:25 +0900

    PGM_알고KIT_완전탐색_카펫

--- Python/Programmers/Kit/full search/carpet.py
+++ Python/Programmers/Kit/full search/carpet.py
@@ -1,9 +1,20 @@
 # 알고리즘 고득점 KIT _ 완전탐색
 # 카펫
 
-
-
 def solution(brown, yellow):
+    # 전체 개수
+    grid = brown + yellow;
+
+    for n in range(3, int(grid ** 0.5) +1):
+        if grid % n !=0: continue
+
+        m = grid // n
+
+        if (n-2) * (m-2) == yellow:
+            return [m, n]
+
+
+def solution2(brown, yellow):
     answer = []
     yellow_x = 0
     yellow_y = 0
