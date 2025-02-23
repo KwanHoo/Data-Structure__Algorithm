0948acd660d107c624667215b2afbbebb98607a3
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2025-02-23 23:02:33 +0900

    PGM_LV1_약수의 합

--- Python/Programmers/LV1/001_sum of divisors.py
+++ Python/Programmers/LV1/001_sum of divisors.py
@@ -4,6 +4,8 @@ def solution(n):
     answer = 0
     # 약수 리스트 생성
     nlist = [1]
+    if n == 0:
+        return 0
 
     for i in range(2, n + 1):
         if n % i == 0:
@@ -11,3 +13,5 @@ def solution(n):
     # 합
     answer = sum(nlist)
     return answer
+
+# 25.02.23 _ testcase 16 : n이 0인 경우 추가
